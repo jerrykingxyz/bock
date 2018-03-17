72013546878cf0a4dfbc23622d169b956cc0cb41
jerrykingxyz <jerrykingxyz@gmail.com>
2018-03-17 21:43:49 +0800

    feat : generate plugin stream

--- src/core/PluginStream.js
+++ src/core/PluginStream.js
@@ -1,7 +1,23 @@
+const path = require('path');
 const Context = require('./Context');
+const { plugins } = global.config;
 
 class PluginStream {
-  constructor (file) {
+
+  constructor (filepath) {
+    const ext = path.extname(filepath).substr(1);
+    this.queue = plugins.filter(e => e.rules === '*' || (e.rules.indexOf(ext) !== -1));
     this.context = new Context();
+    this.index = 0;
+    this.next = this.nextPlugin.bind(this);
   }
+
+  async nextPlugin () {
+    const fn = this.queue[this.index];
+    if (fn) {
+      await fn(this.context, this.next);
+      this.index ++;
+    }
+  }
+
 }
