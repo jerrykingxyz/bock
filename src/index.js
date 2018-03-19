dfcaffc7919450cd2b41b08731b5fe305fd2fb52
jerrykingxyz <jerrykingxyz@gmail.com>
2018-03-19 16:45:36 +0800

    feat : basic version complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const { generateConfig } = require('./config');
+const PluginStream = require('./core/PluginStream');
 
 const lazypack = function (configPath) {
-    global.config = generateConfig(configPath);
+  global.config = generateConfig(configPath);
 
+  for(const item of config.input) {
+    new PluginStream(item);
+  }
 };
 
 module.exports = lazypack;
