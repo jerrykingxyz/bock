3716b60315e3a7d1a88a86635860c34829fa4e13
jerrykingxyz <jerrykingxyz@gmail.com>
2018-03-20 22:52:29 +0800

    fix : plugin stream config getter bug

--- src/core/PluginStream.js
+++ src/core/PluginStream.js
@@ -1,21 +1,20 @@
 const path = require('path');
 const Context = require('./Context');
-const { plugins } = require('../config');
+const config = require('../config');
 
 class PluginStream {
 
   constructor (filepath) {
     const ext = path.extname(filepath).substr(1);
     this.context = new Context(filepath);
-    this.queue = plugins.filter(e => e.rules(ext));
+    this.queue = config.plugins.filter(e => e.rules(ext));
     this.next = this.nextPlugin.bind(this);
   }
 
   async nextPlugin () {
-    const fn = this.queue[0];
-    if (fn) {
-      await fn(this.context, this.next);
-      this.queue.shift();
+    const plugin = this.queue.shift();
+    if (plugin) {
+      await plugin.main(this.context, this.next);
     }
   }
 
