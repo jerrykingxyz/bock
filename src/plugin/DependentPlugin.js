8c7fc199aababd1fdf40569237bf32108cf3e1c5
BenMix <wubendao@gmail.com>
2018-03-25 14:06:10 +0800

    fix : fix  a few bugs of DependentPlugin

--- src/plugin/DependentPlugin.js
+++ src/plugin/DependentPlugin.js
@@ -2,8 +2,8 @@ const path = require('path');
 const defaultRegExp = /(url\(|src=['"]|href=["'])([^'"()]*[?='")])/g;
 
 class DependentPlugin {
-  constructor(regExp = defaultRegExp) {
-      this.setRegExp(regExp);
+  constructor(userExtName) {
+    !!userExtName ? this.setRegExp(userExtName) : this.setRegExp(defaultRegExp);
   }
 
   main(ctx, next) {
@@ -15,29 +15,40 @@ class DependentPlugin {
     let dependentList = [];
     let dependentTemp = ctx.content.match(this.regExp);
 
+    if (dependentTemp === null) return;
+
     dependentTemp.forEach((each) => {
       dependentList.push(new Array(each.split(/['"()]/)[1]));
     });
 
-    if (!!ctx.filepath) {
-      dependentList.forEach(each => {
-        let eachtoString = each.toString();
-        if (!!/^(https|http):\/\//.test(eachtoString)) {
-          return;
-        }
-        each.push(path.join(ctx.filepath, each[0]));
-      });
-    }
-
+    dependentList.forEach(each => {
+      let eachtoString = each.toString();
+      if (/^(https:\/\/|http:\/\/|\/\/)/.test(eachtoString)) {
+        return;
+      }
+      each.push(path.join(ctx.filepath, each[0]));
+    });
     ctx.dependentList = dependentList;
     return next();
 
   }
 
-  setRegExp(userRegExp) {
-    if (toString.call(userRegExp) === '[object RegExp]') {
-      this.regExp = userRegExp;
+  setRegExp(n) {
+    if (toString.call(n) === '[object RegExp]') {
+      return this.regExp = n;
     }
+    this.regExp = this.generateRegExp(n);
+  }
+
+  /**
+   *  @ extNameList [array]
+   */
+  generateRegExp(extNameList) {
+    if (!Array.isArray(extNameList)) {
+      throw new Error('extname must be a array , eg:[\'.css\',\'.htm\']');
+    }
+    return new RegExp(
+        `(url\\(|src=['"]|href=["'])([^'"()]*(${extNameList.join('|')})[?='")])`,'g');
   }
 }
 
--- test/plugin/DependentPlugin.test.js
+++ test/plugin/DependentPlugin.test.js
@@ -7,42 +7,65 @@ const ctx = {
   content: `<img src="https://test.com/test1.png"/><img src='./test/test2.jpg'/>
     <link rel="stylesheet" href="../test/test2.css">
     <img src="http://test.com/test2.png"/>
+      <img src="http://test.com/test2.css"/>
     <div style="background: url(./test/test)"></div> 
     <script src="./test/data.js"></script> 
     <script src="./test1/data"></script> 
+    <script src='//test.com/test' /></script>
+    <script src='//test.com/test.js' /></script>
     </div> `,
   filepath: '/base/demo/',
 };
 
 describe('dependent plugin test', function() {
 
-  let dependentPlugin1 = new DependentPlugin(/testRegExp/);
+  let dependentPlugin1 = new DependentPlugin(['.css', '.js']);
 
   it('setRegExp test', function() {
 
     assert.ok(() => {
       let x = dependentPlugin1.regExp;
-      let y = /testRegExp/;
+      let y = /(url\\(|src=['"]|href=["'])([^'"()]*(.css|.js)[?='")])/g;
       return (x instanceof RegExp) && (y instanceof RegExp) &&
           (x.source === y.source) && (x.global === y.global) &&
           (x.ignoreCase === y.ignoreCase) && (x.multiline === y.multiline);
     });
   });
 
+  it('main function test1', async function() {
+    let nextDone = false;
+    await dependentPlugin1.main(ctx, function() {
+      nextDone = true;
+    });
+    assert.deepEqual(ctx.dependentList, [
+          ['../test/test2.css', '/base/test/test2.css'],
+          ['http://test.com/test2.css'],
+          ['./test/data.js', '/base/demo/test/data.js'],
+          ['//test.com/test.js'],
+        ],
+    );
+    assert.ok(nextDone);
+  });
+
   let dependentPlugin2 = new DependentPlugin();
-  it('main function test', async function() {
+
+  it('main function test2', async function() {
     let nextDone = false;
     await dependentPlugin2.main(ctx, function() {
       nextDone = true;
     });
     assert.deepEqual(ctx.dependentList, [
-      ['https://test.com/test1.png'],
-      ['./test/test2.jpg', '/base/demo/test/test2.jpg'],
-      ['../test/test2.css', '/base/test/test2.css'],
-      ['http://test.com/test2.png'],
-      ['./test/test', '/base/demo/test/test'],
-      ['./test/data.js', '/base/demo/test/data.js'],
-      ['./test1/data', '/base/demo/test1/data']],
+          ['https://test.com/test1.png'],
+          ['./test/test2.jpg', '/base/demo/test/test2.jpg'],
+          ['../test/test2.css', '/base/test/test2.css'],
+          ['http://test.com/test2.png'],
+          ['http://test.com/test2.css'],
+          ['./test/test', '/base/demo/test/test'],
+          ['./test/data.js', '/base/demo/test/data.js'],
+          ['./test1/data', '/base/demo/test1/data'],
+          ['//test.com/test'],
+          ['//test.com/test.js'],
+        ],
     );
     assert.ok(nextDone);
   });
