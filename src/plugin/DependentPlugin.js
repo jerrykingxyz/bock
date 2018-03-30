ac577a1fc5fa3fab2f2606b4ec7bfd8c44820973
BenMix <wubendao@gmail.com>
2018-03-31 00:56:19 +0800

    refact : refact dependentPlugin

--- src/plugin/DependentPlugin.js
+++ src/plugin/DependentPlugin.js
@@ -1,54 +1,97 @@
 const path = require('path');
+//todo: refactor htmlsolve
+const htmlSolve = function(ctx, subConfig) {
+  // (ctx) => {
+  // const regexp = /(url\(|src=['"]|href=["'])([^'"()]*['")])/g;
+  // const dependent = getDependent(ctx.content, regexp);
+
+  // let dependentList = [];
+  // if (dependentTmp) {
+  //   dependentTmp.forEach(obj => {
+  //     let Obj = obj.split(/["'()]/);
+  //     if (Obj[1].match(/(http:\/\/|https:\/\/|\/\/)/)) {
+  //       dependentList.push([Obj[1]]);
+  //     } else {
+  //       let baseDir = getDir(ctx.filepath);
+  //       let relativeDir = Obj[1];
+  //       let absoluteDir = relativeDirToAbsoluteDir(baseDir, relativeDir);
+  //       dependentList.push(
+  //           new Array(relativeDir, absoluteDir);
+  //     )
+  //       ;
+  //     }
+  //
+  //   });
+  // }
+  // return dependentList;
+  //   },
+  // },
+  // };
+};
+
+const subConfigCheck = function(subConfig) {
+
+};
+
+const urlFilter = function(dependent, subConfig) {
+  if (!!subConfig.url) {
+    return dependent;
+  }
+  const dependentFiltered = dependent.filter(obj => {
+    const regexp = /(http:\/\/|https:\/\/|\/\/)/;
+    return !regexp.test(obj);
+  });
+  return dependentFiltered;
+};
+
+const getDependent = function(content, regexp) {
+  return content.match(regexp);
+};
+
+const getDir = function(filePath) {
+  return path.dirname(filePath);
+};
+
+const relativeDirToAbsoluteDir = function(baseDir, relativeDir) {
+  return path.join(baseDir, relativeDir);
+};
 
 const defaultConfig = {
   ext: ['html'],
-  rules: {
-    html: (ctx) => {
-      let regexp = /(url\(|src=['"]|href=["'])([^'"()]*['")])/g;
-      let dependentTmp = ctx.content.match(regexp);
-      let dependentList = [];
-      if (dependentTmp) {
-        dependentTmp.forEach(obj => {
-          let Obj = obj.split(/["'()]/);
-          if (Obj[1].match(/(http:\/\/|https:\/\/|\/\/)/)) {
-            dependentList.push([Obj[1]]);
-          } else {
-            dependentList.push([Obj[1], path.join(path.dirname(ctx.filepath), Obj[1])]);
-          }
-
-        });
-      }
-      return dependentList;
-    },
-  },
-};
-
-function configFormat(config) {
-  let Config = {};
-  if (!!config.ext && config.ext instanceof Array) {
-    Config.ext = config.ext;
-    Config.rules = {};
-  } else {
-    return defaultConfig;
-  }
-  for (let ext of Config.ext) {
-    if (!!config.rules && !!config.rules[ext] &&
-        typeof config.rules[ext] === 'function') {
-      Config.rules[ext] = config.rules[ext];
-    } else if (!!defaultConfig.rules[ext]) {
-      Config.rules[ext] = defaultConfig.rules[ext];
-    } else {
-      throw new Error(
-          `please Set solve function of ${ext} ,this extname don't have default solve function !`);
+  solves: {
+    html: {
+      url: true,
+      solveFunc: htmlSolve
     }
   }
-  return Config;
-}
+};
+// todo: refactor configformat
+// const configFormat = function(config) {
+//   let Config = {};
+//   if (!!config.ext && config.ext instanceof Array) {
+//     Config.ext = config.ext;
+//     Config.solves = {};
+//   } else {
+//     return defaultConfig;
+//   }
+//   for (let ext of Config.ext) {
+//     if (!!config.solves && !!config.solves[ext] &&
+//         typeof config.solves[ext] === 'function') {
+//       Config.solves[ext] = config.solves[ext];
+//     } else if (!!defaultConfig.solves[ext]) {
+//       Config.solves[ext] = defaultConfig.solves[ext];
+//     } else {
+//       throw new Error(
+//           `please Set solve function of ${ext} ,this extname don't have default solve function !`);
+//     }
+//   }
+//   return Config;
+// };
 
 class DependentPlugin {
   constructor(config) {
     this.config = configFormat(config);
-    this.rules = this.config.rules;
+    this.ext = this.config.ext;
   }
 
   main(ctx, next) {
@@ -57,7 +100,7 @@ class DependentPlugin {
       return;
     }
     let extname = path.extname(ctx.filepath).slice(1);
-    ctx.dependent = this.rules[extname](ctx);
+    ctx.dependent = this.config.solves[extname](ctx);
     return next();
   }
 }
